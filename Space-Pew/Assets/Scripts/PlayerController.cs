5f8c835c9824c308af25667470c21e68ec7b2c3d
Patrick Quah <patrick.s.quah@gmail.com>
2014-09-05 16:57:33 -0700

    added ship movement

---
+++ Space-Pew/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	[SerializeField]
+	private float speed=1f;
+	private float horizontalthrust=0f;
+	private float verticalthrust=0f;
+
+	private Rigidbody2D myrigidbody2d;
+
+	void OnEnable () {
+		myrigidbody2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalthrust = Input.GetAxis ("Horizontal");
+		verticalthrust = Input.GetAxis ("Vertical");
+	
+	}
+
+	void FixedUpdate () {
+		Vector2 force = speed * Time.fixedDeltaTime * new Vector2 (horizontalthrust, verticalthrust);
+			//transform.position = transform.position + new Vector3 (speed * Time.fixedDeltaTime * horizontalthrust,0,0);
+		myrigidbody2d.AddForce (force);
+
+	}
+
+
+
+
+}
