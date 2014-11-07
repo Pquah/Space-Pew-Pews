7b84a4abc21c4b8a5aa53c3e6ee00c9e06e07e1f
Patrick Quah <patrick.s.quah@gmail.com>
2014-11-07 15:06:33 -0800

    Added bomb object and behaviour. Moved enemy movement to scripts.

---
+++ Space-Pew/Assets/Scripts/CanExplode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanExplode : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		Explode ();
+	}
+
+	private void Explode ()
+	{
+		//declaring variable
+		//type		 name
+		Collider2D[] hits;
+		hits = Physics2D.OverlapCircleAll ((Vector2)transform.position, 3f);
+		Debug.Log (hits.Length);
+		/*if (hits.Length > 0) {
+			Debug.Log ("I hit something!");
+			DestroyObject (hits [0].gameObject);
+		}*/
+		int destroyed;
+		destroyed = 0;
+		
+		while (destroyed<hits.Length) {
+			DestroyObject (hits [destroyed].gameObject);
+			destroyed ++;
+		}
+	}
+}
+	
+// Update is called once per frame
+
